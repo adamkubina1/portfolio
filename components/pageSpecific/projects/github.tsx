aded99b90381bfc71fe15268fae3ee4359800093
Adam Kubina <adam.kubinaa@gmail.com>
2023-07-17 14:14:13 +0200

    Work on about

--- components/pageSpecific/projects/github.tsx
+++ components/pageSpecific/projects/github.tsx
@@ -1,5 +1,6 @@
 'use client';
-import Image from 'next/image';
+
+//https://github.com/anuraghazra/github-readme-stats
 
 export const Github = () => {
   return (
@@ -8,19 +9,27 @@ export const Github = () => {
       <p className='text-xs md:text-sm text-center text-gray-400 w-1/2'>
         I like my source open
       </p>
-      <img
-        alt='stats'
-        src='http://github-readme-streak-stats.herokuapp.com/?user=adamkubina1&theme=dark&background=000000'
-        className='w-72 bg-white shadow-md shadow-black'
-      />
-      <Image
+      <div className='flex justify-start'>
+        <img
+          alt='stats'
+          src='http://github-readme-streak-stats.herokuapp.com/?user=adamkubina1&theme=transparent&hide_border=true'
+          className='h-full  '
+        />
+        <img
+          alt='stats'
+          src='https://github-readme-stats.vercel.app/api/top-langs/?username=adamkubina1&layout=compact&theme=transparent&hide_border=true'
+          className='h-full'
+        />
+      </div>
+
+      {/* <Image
         alt='stats'
         src={
           'http://github-readme-streak-stats.herokuapp.com?user=adamkubina1&border_radius=4.4&type=png'
         }
         width={200}
         height={72}
-      />
+      /> */}
     </div>
   );
 };
