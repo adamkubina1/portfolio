f4acfa95ba9e49491955c444f55cb117494ab414
adamkubina1 <adam.kubinaa@gmail.com>
2023-09-13 15:00:37 +0200

    Added model

---
+++ components/pageSpecific/home/model.jsx
@@ -0,0 +1,55 @@
+import Spline from '@splinetool/react-spline';
+import { useEffect, useRef } from 'react';
+
+export default function Model() {
+  let initialZoom;
+  const cube = useRef();
+
+  function getScaleValue() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth > 1000) {
+      return 1;
+    } else if (screenWidth > 700) {
+      return 0.9;
+    } else if (screenWidth > 500) {
+      return 0.8;
+    }
+    return 0.7;
+  }
+
+  function onLoad(spline) {
+    const scaleValue = getScaleValue();
+    initialZoom = scaleValue;
+    spline.setZoom(scaleValue);
+    const obj = spline.findObjectByName('Cube');
+    cube.current = obj;
+  }
+
+  function setSplineScale(scale) {
+    const newScale = cube.current.scale;
+    newScale.x = scale;
+    newScale.y = scale;
+    newScale.z = scale;
+    cube.current.scale = newScale;
+  }
+
+  useEffect(() => {
+    function scaleSpline() {
+      if (cube.current?.scale) {
+        const scaleValue = getScaleValue();
+        setSplineScale(scaleValue / initialZoom);
+      }
+    }
+    window.addEventListener('resize', scaleSpline);
+    return () => window.removeEventListener('resize', scaleSpline);
+  }, [initialZoom]);
+
+  return (
+    <div className='h-12'>
+      <Spline
+        onLoad={onLoad}
+        scene='https://prod.spline.design/xzCIRut57GSOeXjc/scene.splinecode'
+      />
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import { PageWrapper } from '@/components/layouts/pageWrapper';
 import { Hero } from '@/components/pageSpecific/home/hero';
-import { Model } from '@/components/pageSpecific/home/model';
+import dynamic from 'next/dynamic';
+
+const Model = dynamic(() => import('@/components/pageSpecific/home/model'), {
+  loading: () => <p>Loading...</p>,
+});
 
 export default function Home() {
   return (
     <PageWrapper direction='right' rightHref='/about'>
       <Hero />
+
       <Model />
     </PageWrapper>
   );
